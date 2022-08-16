cd6a91603c517a4a94967f8ecaf332a996319422
iurysilva <iuryglabson18@gmail.com>
2022-08-16 09:39:05 -0300

    Feat: Adds mocks and loadData service.

---
+++ src/mocks/producers.js
@@ -0,0 +1,46 @@
+import green from "../assets/produtores/green.png"
+import salad from "../assets/produtores/salad.png"
+import jennyJack from "../assets/produtores/jenny-Jack.png"
+import grow from "../assets/produtores/grow.png"
+import potager from "../assets/produtores/potager.png"
+
+const generateRandomNumber = (min, max) =>{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const producers = {
+    title: "Producers",
+    list: [
+        {
+            name: "Green",
+            image: green,
+            distance: '${generateRandomNumber(1, 500)}m',
+            stars: generateRandomNumber(1, 5)
+        },
+        {
+            name: "Jenny Jack",
+            image: jennyJack,
+            distance: '${generateRandomNumber(1, 500)}m',
+            stars: generateRandomNumber(1, 5)
+        },
+        {
+            name: "Salad",
+            image: salad,
+            distance: '${generateRandomNumber(1, 500)}m',
+            stars: generateRandomNumber(1, 5)
+        },
+        {
+            name: "Potager",
+            image: potager,
+            distance: '${generateRandomNumber(1, 500)}m',
+            stars: generateRandomNumber(1, 5)
+        },
+        {
+            name: "Grow",
+            image: grow,
+            distance: '${generateRandomNumber(1, 500)}m',
+            stars: generateRandomNumber(1, 5)
+        },
+        
+    ]
+}
