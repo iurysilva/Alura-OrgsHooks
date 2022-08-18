e96271d8f1d55c4f38547f1e4a5e3efed2738d56
iurysilva <iuryglabson18@gmail.com>
2022-08-18 11:48:52 -0300

    Feat: Sort producers by number of stars

--- src/hooks/useProducers.js
+++ src/hooks/useProducers.js
@@ -3,7 +3,7 @@ import { loadProducers } from "../services/loadData";
 
 const sortProducers = (list) => {
     //return list.sort((a, b) => (a.distance > b.distance) ? 1  : -1)
-    return list.sort((a, b) => (a.stars > b.stars) ? 1  : -1)
+    return list.sort((a, b) => (a.stars < b.stars) ? 1  : -1)
 }
 
 
